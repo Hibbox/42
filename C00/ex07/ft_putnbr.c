172a317bc7513cf92e4c4ed5855d7d1620e060d5
Ronald Mondongue <rmondong@e3r7p12.clusters.42paris.fr>
2022-09-22 19:18:15 +0200

    save 3

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -6,57 +6,44 @@
 /*   By: rmondong <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 22:52:37 by rmondong          #+#    #+#             */
-/*   Updated: 2022/09/22 03:51:54 by rmondong         ###   ########.fr       */
+/*   Updated: 2022/09/22 17:39:41 by rmondong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	my_pow(int n, int p)
+void	ft_putchar(char n)
 {
-	if (n == 0)
-	{
-		return(1);
-	}
-
-	return(my_pow(n, p -1) * n);
+	write(1, &n, 1);
 }
 
 void	ft_putnbr(int nb)
 
 {
-	char	i;
-	int	nb_test;
-	char	nbr;
 	char	moins;
-	
-	moins = '-';
-	i = 0;
+
+	if (nb >= 0 && nb <= 9)
+	{
+		ft_putchar(nb + '0');
+	}
 	if (nb == -2147483648)
 	{
 		write (1, "-2147483648", 12);
-		return;
+		return ;
 	}
 	if (nb < 0)
 	{
 		write(1, &moins, 1);
 		nb = -nb;
 	}
-	nb_test = nb;
-	while (nb_test > 9) 
+	if (nb > 9)
 	{
-		nb_test = nb_test / 10;
-		i++;
-	}
-	while (i > 9)
-	{
-		nbr = (nb / my_pow(10,i)) % 10 + 48;
-		write (1, &nbr, 1);
-		i--;
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
 	}
 }
 
-int main()
+/*int	main(void)
 {
 	ft_putnbr(100);
-}
+}*/

2b99bca04d2f167a85a60f191a15d143b3d81df7
Hibbox <hibooxx9@gmail.com>
2022-11-09 22:39:44 +0100

    update

--- C05/ex05/ft_sqrt.c
+++ C05/ex05/ft_sqrt.c
@@ -1,5 +1,20 @@
+#include <stdio.h>
 
 int ft_sqrt(int nb)
 {
-    
+    int i = 1;
+
+    while(i * i < nb && nb <= 2147395600)
+        i++;
+    if ( i * i == nb)
+        return (i);
+    return 0;
+}
+
+int main ()
+{
+    int i = 46340;
+   /* while(i * i < 2147483647)  permet de chercher la racine carré entière la plus grande pour les int
+        i++;
+    printf("%d\n", ft_sqrt(2147395599)); */
 }
